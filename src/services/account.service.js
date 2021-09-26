2da7fc635b58bb50e3f1b3ebab6bf6b50c82a4db
HungNguyenThac <nthachung@gmail.com>
2021-09-26 08:36:19 +0700

    last update

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,7 +1,7 @@
 const VerifyCodeModel = require("../mongoose/codeVerify.model");
 const userModel = require("../mongoose/user.model");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bycrypt = require("bcrypt");
 const crypto = require("crypto");
 
 exports.userModelFindOneLoginName = (loginName) => {
