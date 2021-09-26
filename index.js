7610f0c6cf2e4c674a8e4f6e38f3b9eaa11d86b5
HungNguyenThac <nthachung@gmail.com>
2021-09-26 10:14:39 +0700

    stable

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/user", routesUser);
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "app/client/build/index.html"));
 });
 
 // Auto wake up heroku
